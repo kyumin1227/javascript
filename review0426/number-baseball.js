c8525a67770447c4ca89ad631a52b14d8dde2fd4
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-04-27 01:01:07 +0900

    Chapter 5

---
+++ review0426/number-baseball.js
@@ -0,0 +1,75 @@
+const $input = document.querySelector("#input");
+const $form = document.querySelector("#form");
+const $logs = document.querySelector("#logs");
+
+const numbers = [];
+
+for (let n = 1; n <= 9; n++) {
+    numbers.push(n);
+}
+
+const answer = [];
+
+for (let n = 0; n <= 3; n++) {
+    const index = Math.floor(Math.random() * numbers.length);
+    answer.push(numbers[index]);
+    numbers.splice(index, 1);
+}
+console.log(answer);
+
+const tries = [];
+
+function checkInput(input) { 
+    if (input.length !== 4) {
+        return alert("4자리 숫자를 입력해주세요")
+    }
+    if (new Set(input).size !== 4) {
+        return alert("중복되지 않게 입력해 주세요");
+    }
+    if (tries.includes(input)) {
+        return alert("이미 시도한 값입니다.");
+    }
+    return true;
+}
+
+$form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const value = $input.value;
+    $input.value = "";
+    const valid = checkInput(value);
+    if (!valid) return;
+    if (answer.join("") === value) {
+        $logs.textContent = "홈런!";
+        return;
+    }
+    if (tries.length >= 9) {
+        const message = document.createTextNode(`패배! 정답은 ${answer.join("")}`);
+        $logs.appendChild(message);
+        return;
+    }
+
+    let strike = 0;
+    let ball = 0;
+    for (let i = 0; i < 4; i++) {
+        const index = value.indexOf(answer[i]);
+        if (i == index) {
+            strike += 1;
+        } else if (index > -1) {
+            ball += 1;
+        }
+    }
+    // for (let i = 0; i < 4; i++) {
+    //     if (value[i] == answer[i]) {
+    //         strike++;
+    //         continue;
+    //     } else {
+    //         for (let y = 0; y < 4; y++) {
+    //             if (value[i] == answer[y]) {
+    //                 ball++;
+    //             }
+    //         }
+    //     }
+    // }
+    $logs.append(`${value}: ${strike} 스트라이크 ${ball} 볼`, document.createElement("br"));
+    tries.push(value);
+})
