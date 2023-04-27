29331183e9834dd2a50b37cda6f7ae19789da210
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-04-27 19:22:24 +0900

    Test

---
+++ test/test0427.js
@@ -0,0 +1,59 @@
+// if문 switch문과 삼항 연산자로 바꿔보기
+
+// let cond = true;
+// let value = "";
+
+// if (cond) {
+//     value = "참";
+// } else {
+//     value = "거짓";
+// }
+
+// console.log(value);
+
+// switch (cond) {
+//     case true:
+//         value = "참";
+//         break;
+//     case false:
+//         value = "거짓";
+//         break;
+// }
+
+// console.log(value);
+
+// value = cond ? "참" : "거짓";
+
+// console.log(value);
+
+// 100번 출력
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(i + 1);
+// }
+
+// continue문 테스트
+
+// let i = 0;
+// while (i < 50) {
+//     i++
+//     if (i % 2 == 1) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// 구구단을 출력하나 결과에 짝수가 나오지 않도록
+
+// for (let i = 2; i < 10; i++) {
+//     if (i % 2 == 0) {
+//         continue;
+//     }
+//     for (let j = 2; j < 10; j++) {
+//         if (j % 2 == 0) {
+//             continue;
+//         }
+//         console.log(`${i} * ${j} = ${i * j}`);
+//     }
+// }
+
